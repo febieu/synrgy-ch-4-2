8de7b6f9a1d02034694908d296894a1a9a30b4c7
febieu <febiyantiw96@gmail.com>
2023-10-23 11:56:03 +0700

    add login activity

---
+++ app/src/main/java/com/example/synrgychallenge4/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.example.synrgychallenge4
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val editTextName = findViewById<EditText>(R.id.edittext_login)
+        val editTextPassword = findViewById<EditText>(R.id.edittext_password)
+        val button = findViewById<Button>(R.id.button_login)
+        
+        button.setOnClickListener {
+            val username = editTextName.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if(username.isEmpty() || password.isEmpty()){
+                Toast.makeText(applicationContext, "Username dan Password Harus di Isi!", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val sharedPreferences = getSharedPreferences("MYPREFERRENCES", Context.MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+                editor.putString("USERNAME", username)
+                editor.putString("PASSWORD", password)
+                editor.apply()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
